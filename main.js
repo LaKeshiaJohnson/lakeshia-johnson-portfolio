b77cf80e815973d215a4735ee5b64ad79e6cea81
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-11-29 16:25:48 -0600

    added new modal for projects with modal content

--- main.js
+++ main.js
@@ -20,12 +20,12 @@ function myMap()
 
 
 // Modal Image Gallery
-function onClick(element) {
+/*function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
-}
+}*/
 
 
 // Change style of navbar on scroll
@@ -47,5 +47,27 @@ function toggleFunction() {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+//modal 1
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
