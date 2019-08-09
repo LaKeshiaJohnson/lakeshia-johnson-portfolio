522863edf41f82b62087cea4e227439361b6f5e7
lakeshia <ljohnson2k@gmail.com>
2019-08-09 12:43:12 -0500

    added magic8ball project modal, removed orginal paralax image, added optimized paralax image

--- main.js
+++ main.js
@@ -118,6 +118,26 @@ window.onclick = function(event) {
     }
 }
 
+//MAGIC8BALL - modal 4
+var modal4 = document.getElementById('myModal4');
+var btn4 = document.getElementById("myBtn4");
+var span4 = document.getElementsByClassName("close4")[0];
 
+// When the user clicks on the button, open the modal 
+btn4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+    modal4.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal4) {
+        modal4.style.display = "none";
+    }
+}
 
 
